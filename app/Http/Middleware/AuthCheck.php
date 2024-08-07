d33e047b5a50d44a6f03d3c63dcde1950134b61c
test <test@gmail.com>
2024-08-07 21:55:04 +0530

    changes of theme

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -21,15 +21,15 @@ class AuthCheck
         if (Auth::user() == 'null' || empty(Auth::user()) || Auth::user()  == '' || Auth::user()  == null) {
             return route('login');
         } else {
-            if (auth()->user()->main_user_id == 0) {
-                $application = Application::where('user_id', auth()->user()->id)->first();
-                if (!is_null($application)) {
-                    if ($application->status == 4 || $application->status == 5 || $application->status == 6) {
-                    }
-                } else {
-                    return redirect()->route('dashboardPage');
-                }
-            }
+            // if (auth()->user()->main_user_id == 0) {
+            //     $application = Application::where('user_id', auth()->user()->id)->first();
+            //     if (!is_null($application)) {
+            //         if ($application->status == 4 || $application->status == 5 || $application->status == 6) {
+            //         }
+            //     } else {
+            //         return redirect()->route('dashboardPage');
+            //     }
+            // }
         }
         return $next($request);
     }
